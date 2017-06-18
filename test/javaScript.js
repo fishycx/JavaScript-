71429ac75ad4bb2a7ce45dceedd1cb374d9f5774
fishycx <fishycx65@gmail.com>
2017-06-18 23:20:36 +0800

    javaScript文档学习记录

---
+++ test/javaScript.js
@@ -0,0 +1,86 @@
+/**
+ * Created by yuchuanxing on 2017/6/17.
+ */
+var x = 2;
+var y = 3;
+var z = x + y;
+/*大小写敏感*/
+/*您可以在一条语句中声明很多变量。该语句以 var 开头，并使用逗号分隔变量即可：
+ */
+/*Value = undefined
+ 在计算机程序中，经常会声明无值的变量。未使用值来声明的变量，其值实际上是 undefined。
+ 在执行过以下语句后，变量 carname 的值将是 undefined：
+ */
+/*重新声明 JavaScript 变量
+ 如果重新声明 JavaScript 变量，该变量的值不会丢失：
+ 在以下两条语句执行后，变量 carname 的值依然是 "Volvo"：
+ */
+
+
+/*-----------------------------------------------------------------------------*/
+
+
+/*数据类型*/
+/*JavaScript 拥有动态类型。这意味着相同的变量可用作不同的类型：
+ */
+var  x ; var x = 6; var x = "Bill"
+
+//javaScript 函数
+/*向未声明的 JavaScript 变量来分配值
+ 如果您把值赋给尚未声明的变量，该变量将被自动作为全局变量声明。
+ */
+
+
+//javaScript错误-Throw、Try和catch
+/*try 语句测试代码块的错误。
+ catch 语句处理错误。
+ throw 语句创建自定义错误。
+ */
+try
+{
+    //在这里运行代码
+}
+catch(err)
+{
+    //在这里处理错误
+}
+
+/*javaScript HTML DOM
+* 1.改变HTML输入流
+* 2.改变HTML内容
+* 3.改变HTML的属性
+* */
+
+
+/*改变CSS
+* 1.改变HTML样式
+* document.getElementById(id).style.property=new style
+*
+ * */
+
+/*
+* DOM事件
+*HTML DOM 允许您通过使用 JavaScript 来向 HTML 元素分配事件：
+*
+ * */
+
+/*
+* onload and onunload
+* onload 和 onunload 事件会在用户进入或离开页面时被触发。
+* onload 事件可用于检测访问者的浏览器类型和浏览器版本，并基于这些信息来加载网页的正确版本。
+* onload 和 onunload 事件可用于处理 cookie。
+ * */
+
+/*
+* onchange
+* onchange 事件常结合对输入字段的验证来使用。
+ * */
+
+/*
+* onmouseover onmouseout
+* onmouseover 和 onmouseout 事件可用于在用户的鼠标移至 HTML 元素上方或移出元素时触发函数。
+ * */
+
+/*
+* dom 元素
+* */
